bef5d47cb51031577a93aee5530fcc81e74011ee
ELCOT <ELCOT@DESKTOP-UOM23Q0>
2024-08-04 10:10:32 -0700

    final

--- selva/src/selva/Banksystem.java
+++ selva/src/selva/Banksystem.java
@@ -1,25 +1,49 @@
 package selva;
 
+import java.util.Scanner;
+
 public class Banksystem {
     public static void main(String[] args) {
-        Account selvaAccount = new Account(1000);
+    	
+        Account selvaAccount = new Account(100000);
+        System.out.println("1. deposit  2. withdraw  3.check balance");
+        
+        int a,b,c;
+        System.out.println("enter amount to deposit");
+        Scanner sc=new Scanner(System.in);
+       String  aa=sc.nextLine();
+switch(aa) {
+case "1":
+	Scanner add=new Scanner(System.in);
+	System.out.println("enter amount to deposit ");
+	a=add.nextInt();
+	Thread depositThread1 = new DepositThread(selvaAccount, a);
+	 depositThread1.start();
+	break;
+case "2":
+	Scanner mul=new Scanner(System.in);
+System.out.println("enter amount to withdra");
+b=mul.nextInt();
+Thread withdrawThread1 = new WithdrawThread(selvaAccount, b);
+withdrawThread1.start();
+	break;
+case "3":
+	 System.out.println("Final balance: " + selvaAccount.getBalance());
+	break;
+	default:
+		System.out.println("SORRY, can u check ,r u enter correct option or tryagain later ");
+}
 
-        Thread depositThread1 = new DepositThread(selvaAccount, 500);
-        Thread withdrawThread1 = new WithdrawThread(selvaAccount, 300);
-        Thread withdrawThread2 = new WithdrawThread(selvaAccount, 800);
+      
+    
+        //try {
+       //     depositThread1.join();
+       //     withdrawThread1.join();
+          
+     //   } catch (InterruptedException e) {
+      //      e.printStackTrace();
+     //   }
 
-        depositThread1.start();
-        withdrawThread1.start();
-        withdrawThread2.start();
-
-        try {
-            depositThread1.join();
-            withdrawThread1.join();
-            withdrawThread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Final balance: " + selvaAccount.getBalance());
+      
     }
 }
